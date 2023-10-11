b7629006120a1b90e50dd8973ada64b7f5780b2f
Emre YÜKSEL <equesse78@gmail.com>
2023-10-11 15:37:26 +0300

    Add project files.

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using Northwind.DataAcces.Concrete;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager
+    {
+        ProductDal _productDal = new ProductDal();
+
+        public List<Product> GetAll()
+        {
+            //Business Code
+            ProductDal productDal = new ProductDal();
+            return _productDal.GetAll();
+        }
+
+    }
+}
