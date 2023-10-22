05fa6544c3adacb41485e0e5f21272de20495498
Emre YÜKSEL <equesse78@gmail.com>
2023-10-22 22:40:00 +0300

    a

--- Northwind.Entities/Concrete/Product.cs
+++ Northwind.Entities/Concrete/Product.cs
@@ -10,6 +10,7 @@ namespace Northwind.Entities.Concrete
     public class Product:IEntity
     {
         public int ProductId { get; set; }
+        
         public string ProductName { get; set; }
         public int CategoryId { get; set; }
         public decimal UnitPrice { get; set; }
